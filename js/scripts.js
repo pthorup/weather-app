a84cc285002119003cdb856648b82ac6a52d7f29
Poonam Thorup <poonam.thorup@gmail.com>
2021-02-17 15:02:47 +0100

    remove old exchange app code

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,3 @@
 document.addEventListener('DOMContentLoaded', function () {
   const WeatherApp = new WeatherController(new WeatherModel(), new WeatherView());
-  const ExchangeRate = new ExchangeRateController(new ExchangeRateModel(), new ExchangeRateView());
 });
