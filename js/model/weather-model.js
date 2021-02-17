2de96a508ea31fb3876b1044cd95ceb181ef96c4
Poonam Thorup <poonam.thorup@gmail.com>
2021-02-17 15:06:05 +0100

    api link updated

--- js/model/weather-model.js
+++ js/model/weather-model.js
@@ -10,7 +10,7 @@ class WeatherModel {
 
   async getWeatherData(cityID) {
     const apiKey = 'f70f50822fc7d944611cb3493836c135';
-    const apiUrl = `http://api.openweathermap.org/data/2.5/forecast?id=${cityID}&units=metric&lang=sv&appid=${apiKey}`;
+    const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?id=${cityID}&units=metric&lang=sv&appid=${apiKey}`;
 
     const responseWeather = await fetch(apiUrl);
     const weatherData = await responseWeather.json();
